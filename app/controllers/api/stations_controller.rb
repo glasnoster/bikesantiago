8c1c4d3e831c267c07e63f4e8a05cb021603b9bd
Martin Pretorius <glasnoster@gmail.com>
2019-04-06 17:17:44 -0300

    Search stations by comuna

--- app/controllers/api/stations_controller.rb
+++ app/controllers/api/stations_controller.rb
@@ -1,15 +1,24 @@
 class Api::StationsController < ApplicationController
   def index
-    @stations = if !search_params.empty?
-      name_search = search_params[:name]
-      Station.by_name(name_search)
-    else
-      Station.all
-    end
+    @stations = search_params.empty? ? all_stations : search_stations
   end
 
   private
   def search_params
-    @search_params ||= params.permit(:name)
+    @search_params ||= params.permit(:name, :comuna)
+  end
+
+  def all_stations
+    Station.all
+  end
+
+  def search_stations
+    if search_params[:name]
+      Station.by_name(search_params[:name])
+    elsif search_params[:comuna]
+      Station.joins(:comuna)
+        .where(comuna: Comuna.by_name(search_params[:comuna]))
+        .includes(:comuna)
+    end
   end
 end
--- spec/controllers/api/stations_controller_spec.rb
+++ spec/controllers/api/stations_controller_spec.rb
@@ -10,13 +10,19 @@ RSpec.describe Api::StationsController do
 
     context "with search params" do
       let(:name_param) { "Providencia" }
+      let(:comuna_param) { "Vitacura" }
+
       it "filters by name when a name is supplied" do
         expect(Station).to receive(:by_name).with(name_param)
 
         get :index, params: {name: name_param}
       end
 
-      it "filters by comuna when a comuna is supplied"
+      it "filters by comuna when a comuna is supplied" do
+        expect(Comuna).to receive(:by_name).with(comuna_param)
+
+        get :index, params: {comuna: comuna_param}
+      end
     end
   end
 end
