19a6b61b015e26d005a8ba38c63b933cbecddcb6
Martin Pretorius <glasnoster@gmail.com>
2019-04-05 22:40:17 -0300

    Add FactoryBot to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,11 @@ require 'coveralls'
 Coveralls.wear!
 
 require 'webmock/rspec'
+require 'factory_bot'
 
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
