9c7c87486a10d2a8012aa7e03d3fce225aadc3b2
Martin Pretorius <glasnoster@gmail.com>
2019-04-05 22:40:48 -0300

    Stations controller can search by name

--- app/controllers/api/stations_controller.rb
+++ app/controllers/api/stations_controller.rb
@@ -1,5 +1,15 @@
 class Api::StationsController < ApplicationController
   def index
-    render json: []
+    @stations = if !search_params.empty?
+      name_search = search_params[:name]
+      Station.by_name(name_search)
+    else
+      Station.all
+    end
+  end
+
+  private
+  def search_params
+    @search_params ||= params.permit(:name)
   end
 end
--- spec/controllers/api/stations_controller_spec.rb
+++ spec/controllers/api/stations_controller_spec.rb
@@ -1,11 +1,21 @@
 RSpec.describe Api::StationsController do
   describe "GET index" do
     context "without params" do
-      it "gets all the stations"
+      it "gets all the stations" do
+        expect(Station).to receive(:all)
+
+        get :index
+      end
     end
 
     context "with search params" do
-      it "filters by name when a name is supplied"
+      let(:name_param) { "Providencia" }
+      it "filters by name when a name is supplied" do
+        expect(Station).to receive(:by_name).with(name_param)
+
+        get :index, params: {name: name_param}
+      end
+
       it "filters by comuna when a comuna is supplied"
     end
   end
