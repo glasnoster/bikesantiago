bcfc86c6f5cb56d8d7b962100f2f939551bb0e3b
Martin Pretorius <glasnoster@gmail.com>
2019-04-07 12:37:01 -0400

    StationLog can generate usage reports

--- app/models/station_log.rb
+++ app/models/station_log.rb
@@ -2,8 +2,19 @@ class StationLog < ApplicationRecord
   belongs_to :station
   validates :station, presence: true
 
-  def self.usage(stations, last=:hour)
+  def self.report_for(stations, last=:hour)
+    now = Time.now
+    time_window = last == :hour ? (now - 1.hours)..now : (now - 1.day)..now
+    trunc_res = last == :hour ? 'minute' : 'hour'
 
+    StationLog.joins(:station)
+      .where(station: stations)
+      .where(updated_at: time_window)
+      .select(%{
+        DATE_TRUNC('%s', station_logs.created_at) as timestamp,
+        SUM(free_bikes) as total_free_bikes,
+        SUM(empty_slots) as total_empty_slots} % trunc_res)
+      .group("timestamp")
+      .order("timestamp")
   end
-
 end
--- spec/models/station_log_spec.rb
+++ spec/models/station_log_spec.rb
@@ -1,3 +1,49 @@
+RSpec.shared_examples "a usage report" do
+  before do
+    station1 = create(:station, name: "Station 1")
+    station2 = create(:station, name: "Station 2")
+
+    create(:station_log, station: station1, created_at: t1, free_bikes: 1, empty_slots: 2)
+    create(:station_log, station: station2, created_at: t1, free_bikes: 2, empty_slots: 2)
+    create(:station_log, station: station1, created_at: t2, free_bikes: 1, empty_slots: 1)
+  end
+
+  it "groups the timestamps into minute buckets" do
+    expect(StationLog.report_for(Station.all, last).map(&:timestamp).count).to eq(2)
+  end
+
+  it "sums the number of free bikes in each slot" do
+    expect(StationLog.report_for(Station.all, last).map(&:total_free_bikes)).to eq([1, 3])
+  end
+
+  it "sums the number of empty slots in each slot" do
+    expect(StationLog.report_for(Station.all, last).map(&:total_empty_slots)).to eq([1, 4])
+  end
+
+  it "filters by station" do
+    s = Station.where(name: "Station 2")
+    expect(StationLog.report_for(s, last).map(&:timestamp).count).to eq(1)
+  end
+end
+
 RSpec.describe StationLog do
+  describe "#report_for" do
+
+    context "the last hour" do
+      let(:t1) { Time.now }
+      let(:t2) { t1 - 2.minutes }
+      let(:last) { :hour }
+
+      it_behaves_like "a usage report"
+    end
+
+    context "the last day" do
+      let(:t1) { Time.now }
+      let(:t2) { t1 - 2.hours }
+      let(:last) { :day }
+
+      it_behaves_like "a usage report"
+    end
+  end
 
 end
